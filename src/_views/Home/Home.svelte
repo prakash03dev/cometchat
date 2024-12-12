80be14667e7f93fc8c58f6d12928f7e343b9ebdd
prakash03yes <prakash03.yes@gmail.com>
2024-12-12 22:08:07 +0530

    clients

--- src/_views/Home/Home.svelte
+++ src/_views/Home/Home.svelte
@@ -1,8 +1,10 @@
 <script>
 	import Page from '@/_comps/layouts/Page.svelte';
 	import Banner from './_sections/Banner/Banner.svelte';
+	import Clients from './_sections/Clients/Clients.svelte';
 </script>
 
 <Page>
 	<Banner />
+	<Clients />
 </Page>
---
+++ src/_views/Home/_sections/Clients/Clients.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Container from '@/_comps/layouts/Container.svelte';
+	import Section from '@/_comps/layouts/Section.svelte';
+</script>
+
+<Section className="bg-grayscale_dark_01">
+	<Container>Clients</Container>
+</Section>
