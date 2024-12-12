f6cde1e665f56df34b37f0b47226c1767470c3b1
kesav <kesav424@gmail.com>
2024-12-12 22:07:12 +0530

    pushed

--- src/_views/Home/Home.svelte
+++ src/_views/Home/Home.svelte
@@ -1,8 +1,10 @@
 <script>
 	import Page from '@/_comps/layouts/Page.svelte';
 	import Banner from './_sections/Banner/Banner.svelte';
+	import Partner from './_sections/Partner/Partner.svelte';
 </script>
 
 <Page>
 	<Banner />
+	<Partner/>
 </Page>
---
+++ src/_views/Home/_sections/Partner/_icons/index.ts
@@ -0,0 +1,23 @@
+import CreaditIcon from "./CreaditIcon.svelte";
+import  DeveloperIcon from "./DeveloperIcon.svelte";
+import IncomeIcon from "./IncomeIcon.svelte";
+import KnowledgeIcon from "./KnowledgeIcon.svelte";
+import ShareIcon from "./ShareIcon.svelte";
+import SupportIcon from "./SupportIcon.svelte";
+import TimeIcon from "./TimeIcon.svelte";
+import TrainingIcon from "./TrainingIcon.svelte";
+import ValueIcon from "./ValueIcon.svelte";
+
+
+
+export {
+    CreaditIcon,
+    DeveloperIcon,
+    IncomeIcon,
+    KnowledgeIcon,
+    ShareIcon,
+    SupportIcon,
+    TimeIcon,
+    TrainingIcon,
+    ValueIcon 
+}
