eed6076ed73f1427b0e47cd3dbbb05213cc01833
prakash03dev <prakash03.dev@gmail.com>
2024-12-12 09:19:45 +0530

    test done

---
+++ src/_comps/typography/H1.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { type Snippet } from 'svelte';
+
+	interface Props {
+		className?: string;
+		children: Snippet;
+	}
+
+	let { className = '', children }: Props = $props();
+</script>
+
+<h1 class={'text-2xl text-purple-500' + className}>
+	{@render children()}
+</h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import H1 from '../_comps/typography/H1.svelte';
+</script>
+
+<H1>Testing h1</H1>
