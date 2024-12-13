8319380b4f653776b5b1e95e6552729d90192c85
prakash03yes <prakash03.yes@gmail.com>
2024-12-13 19:05:24 +0530

    banner animated

--- src/_views/Home/_sections/Banner/_comps/BgCommets/BgBigComets.svelte
+++ src/_views/Home/_sections/Banner/_comps/BgCommets/BgBigComets.svelte
@@ -1,13 +1,60 @@
-<script>
+<script lang="ts">
 	import { cn } from '@/_utils/tailwind.utils';
 	import BlueCommet from './_comps/BlueComet.svelte';
 	import RedCommet from './_comps/RedComet.svelte';
 	import OrangeCommet from './_comps/OrangeComet.svelte';
 	import BlueMiniComet from './_comps/BlueMiniComet.svelte';
+	import { onMount } from 'svelte';
+	import gsap from 'gsap';
+
+	let blueCommet: HTMLElement;
+	let blueMiniCommet: HTMLElement;
+	let redMiniCommet: HTMLElement;
+	let orangeMiniCommet: HTMLElement;
+
+	onMount(() => {
+		const blueCometTimeline = gsap.timeline({ repeat: -1 });
+		blueCometTimeline.to(blueCommet, {
+			keyframes: {
+				y: ['10%', '0%', '10%']
+			},
+			duration: 20,
+			yoyo: true
+		});
+
+		const blueMiniCommetTimeline = gsap.timeline({ repeat: -1, reversed: true });
+		blueMiniCommetTimeline.to(blueMiniCommet, {
+			keyframes: {
+				y: ['10%', '0%', '10%']
+			},
+			duration: 5,
+			yoyo: true
+		});
+
+		const redMiniCommetTimeline = gsap.timeline({ repeat: -1 });
+		redMiniCommetTimeline.to(redMiniCommet, {
+			keyframes: {
+				y: ['5%', '0%', '5%']
+			},
+			duration: 10,
+			yoyo: true
+		});
+
+		const orangeMiniCommetTimeline = gsap.timeline({ repeat: -1 });
+		orangeMiniCommetTimeline.to(orangeMiniCommet, {
+			keyframes: {
+				scale: [1.2, 1, 1.2]
+			},
+
+			duration: 10,
+			yoyo: true
+		});
+	});
 </script>
 
 <div class="flex flex-col" data-name="big-commets">
 	<div
+		bind:this={blueCommet}
 		data-name="blue-comet"
 		class={cn(
 			['absolute left-0 bottom-0 -z-2 -translate-x-1/2'],
@@ -19,6 +66,7 @@
 	</div>
 
 	<div
+		bind:this={blueMiniCommet}
 		data-name="blue-mini-comet"
 		class={cn(
 			['hidden absolute right-1/2 bottom-0 -z-2 translate-x-[55%] translate-y-[23%]'],
@@ -29,6 +77,7 @@
 	</div>
 
 	<div
+		bind:this={redMiniCommet}
 		data-name="red-comet"
 		class={cn(['absolute right-0 bottom-0 -z-2 translate-x-[40%] translate-y-[40%]'])}
 	>
@@ -36,6 +85,7 @@
 	</div>
 
 	<div
+		bind:this={orangeMiniCommet}
 		data-name="orange-comet"
 		class={cn(
 			['hidden absolute left-0 bottom-0 -z-2', '-translate-x-[30%] translate-y-[40%]'],
--- src/_views/Home/_sections/Banner/_comps/BgCommets/BgSmallComets.svelte
+++ src/_views/Home/_sections/Banner/_comps/BgCommets/BgSmallComets.svelte
@@ -1,25 +1,66 @@
-<script>
+<script lang="ts">
 	import { cn } from '@/_utils/tailwind.utils';
 	import TinyComet from './_comps/TinyComet.svelte';
 	import MiniComet from './_comps/MiniComet.svelte';
 	import SmallComet from './_comps/SmallComet.svelte';
+	import { onMount } from 'svelte';
+	import gsap from 'gsap';
+
+	let tinyCommet: HTMLElement;
+	let miniCommet: HTMLElement;
+	let smallCommet: HTMLElement;
+
+	onMount(() => {
+		const tinyCommetTimeline = gsap.timeline({ repeat: -1 });
+		tinyCommetTimeline.to(tinyCommet, {
+			keyframes: {
+				y: ['100%', '0%', '100%'],
+				x: ['50%', '100%', '0%', '50%']
+			},
+			duration: 5,
+			yoyo: true
+		});
+
+		const miniCommetTimeline = gsap.timeline({ repeat: -1 });
+		miniCommetTimeline.to(miniCommet, {
+			keyframes: {
+				y: ['10%', '0%', '10%'],
+				x: ['10%', '0%', '10%']
+			},
+			duration: 10,
+			yoyo: true
+		});
+
+		const smallCommetTimeline = gsap.timeline({ repeat: -1 });
+		smallCommetTimeline.to(smallCommet, {
+			keyframes: {
+				y: ['10%', '0%', '10%'],
+				x: ['50%', '-80%', '0%', '50%']
+			},
+			duration: 8,
+			yoyo: true
+		});
+	});
 </script>
 
 <div class="flex flex-col" data-name="small-commets">
 	<!-- desktop comets -->
 	<div
+		bind:this={tinyCommet}
 		data-name="tiny-comet"
 		class={cn(['hidden absolute top-[23%] left-[10%] -z-5'], ['lg:block'])}
 	>
 		<TinyComet />
 	</div>
 	<div
+		bind:this={miniCommet}
 		data-name="mini-comet"
-		class={cn(['hidden absolute top-[24%] right-[10%] -z-5'], ['lg:block'])}
+		class={cn(['hidden absolute top-[20%] right-[5%] -z-5'], ['lg:block'])}
 	>
 		<MiniComet />
 	</div>
 	<div
+		bind:this={smallCommet}
 		data-name="small-comet"
 		class={cn(['hidden absolute top-[10%] left-[0%] -z-5'], ['lg:block'])}
 	>
