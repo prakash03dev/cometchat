cf7f9e042587772313526e476eab3bfdd34141d8
prakash03yes <prakash03.yes@gmail.com>
2024-12-13 06:34:18 +0530

    clients done

--- src/_views/Home/_sections/Clients/Clients.svelte
+++ src/_views/Home/_sections/Clients/Clients.svelte
@@ -1,8 +1,29 @@
-<script>
+<script lang="ts">
+	import { cn } from '@/_utils/tailwind.utils';
 	import Container from '@/_comps/layouts/Container.svelte';
 	import Section from '@/_comps/layouts/Section.svelte';
+	import emblaCarouselSvelte from 'embla-carousel-svelte';
+	import AutoScroll from 'embla-carousel-auto-scroll';
+	import { clientsData } from './_data/clients.data';
+
+	const options = {
+		loop: true,
+		watchDrag: false,
+		watchFocus: false
+	};
+	const plugins = [AutoScroll({ speed: 1, stopOnInteraction: false, stopOnFocusIn: false })];
 </script>
 
 <Section className="bg-grayscale_dark_01">
-	<Container>Clients</Container>
+	<Container expand="full" pxEnabled={false}>
+		<div class={cn(['embla', 'overflow-hidden'])} use:emblaCarouselSvelte={{ options, plugins }}>
+			<div class="embla__container flex items-center">
+				{#each clientsData as client}
+					<div class="embla__slide min-w-0 mx-5 max-w-full flex-[0_0_auto]">
+						<img class="object-contain h-full" src={client.img} alt={client.alt} />
+					</div>
+				{/each}
+			</div>
+		</div>
+	</Container>
 </Section>
---
+++ src/_views/Home/_sections/Clients/_data/clients.data.ts
@@ -0,0 +1,59 @@
+import client1 from "../_assets/client1.svg";
+import client2 from "../_assets/client2.svg";
+import client3 from "../_assets/client3.svg";
+import client4 from "../_assets/client4.svg";
+import client5 from "../_assets/client5.svg";
+import client6 from "../_assets/client6.svg";
+
+
+
+export const clientsData = [
+    {
+        alt: "client1",
+        img: client1,
+    },
+    {
+        alt: "client2",
+        img: client2,
+    },
+    {
+        alt: "client3",
+        img: client3,
+    },
+    {
+        alt: "client4",
+        img: client4,
+    },
+    {
+        alt: "client5",
+        img: client5,
+    },
+    {
+        alt: "client6",
+        img: client6,
+    },
+    {
+        alt: "client1",
+        img: client1,
+    },
+    {
+        alt: "client2",
+        img: client2,
+    },
+    {
+        alt: "client3",
+        img: client3,
+    },
+    {
+        alt: "client4",
+        img: client4,
+    },
+    {
+        alt: "client5",
+        img: client5,
+    },
+    {
+        alt: "client6",
+        img: client6,
+    },
+]
